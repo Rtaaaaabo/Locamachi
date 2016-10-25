227dc7633561b0450709b9f3713f88d6d2ab130b
Taku <r.taaaaabo@gmail.com>
2016-10-25 20:51:07 +0900

    タブ時の挙動

--- controller/LocationCtrl.js
+++ controller/LocationCtrl.js
@@ -1,11 +1,9 @@
 angular.module('app.locationTabController', [])
 
 .controller('LocationTabCtrl', function($scope) {
-  function initialize() {
-    $scope.active_content = 'cafe';
-    $scope.setActiveContent = function(active_content) {
-      $scope.active_content = active_content;
-    }
+  $scope.active_content = 'cafe';
+  $scope.setActiveContent = function(active_content) {
+    $scope.active_content = active_content;
     var myLatlng = new google.maps.LatLng(35.630442, 139.882951);
     var mapOptions = {
       zoom : 15,
@@ -13,36 +11,31 @@ angular.module('app.locationTabController', [])
       mapTypeId : google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var infowindow = new google.maps.InfoWindow();
+    //var infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
       location : myLatlng,
       radius : 2000,
       types : [$scope.active_content]
     }, callback);
-
     function callback (results, status) {
       if(status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
           createMarker(results[i]);
         }
       }
-    };
-
+    }
     function createMarker(place) {
       var placeLoc = place.geometry.location;
       var marker = new google.maps.Marker({
         map : map,
-        position : place.geometry.location,
-        category : $scope.active_content
+        position : place.geometry.location
       });
-      google.maps.event.addListener(marker, 'click', function() {
+      /*google.maps.event.addListener(marker, 'click', function() {
         infowindow.setContent(place.name);
         infowindow.open(map, marker);
-      })
+      })*/
     };
+      console.log($scope.active_content);
   }
-
-  //Init Map
-  initialize();
 })
