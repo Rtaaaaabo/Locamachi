fcc63f896f6ffb5f9de79f553ed6d44b9eec3b8d
Taku <r.taaaaabo@gmail.com>
2016-10-29 01:01:53 +0900

    無限ループの追加

--- controller/GuideCtrl.js
+++ controller/GuideCtrl.js
@@ -1,35 +1,44 @@
 angular.module('app.guideTabController', [])
+
 .controller('GuideTabCtrl', function($scope){
-  $scope.items = [
-  {
-    id:0,
-    title : '古川未鈴',
-    subtitle : '歌って踊れるゲーマーアイドル'
-  },
-  {
-    id:1,
-    title : '相沢梨紗',
-    subtitle : '2.5次元伝説！'
-  },
-  {
-    id:2,
-    title : '夢眠ねむ',
-    subtitle : '永遠の魔法少女未満'
-  },
-  {
-    id : 3,
-    title : '成瀬瑛美',
-    subtitle : 'ハイテンションA-POPガール'
-  },
-  {
-    id : 4,
-    title : '最上もが',
-    subtitle : '金色の異端児'
-  },
-  {
-    id : 5,
-    title : '藤咲彩音',
-    subtitle : '踊ってみたら七変化！'
-  }
+  $scope.moredata = false;
+  $scope.loadMoreData = function() {
+    $scope.items.push({title: $scope.items.length});
+    if($scope.items.length==100){
+      $scope.moredata = true;
+    }
+    $scope.$broadcast('scroll.infiniteScrollComplete');
+  };
+    $scope.items = [
+    {
+      id:0,
+      title : '古川未鈴',
+      subtitle : '歌って踊れるゲーマーアイドル'
+    },
+    {
+      id:1,
+      title : '相沢梨紗',
+      subtitle : '2.5次元伝説！'
+    },
+    {
+      id:2,
+      title : '夢眠ねむ',
+      subtitle : '永遠の魔法少女未満'
+    },
+    {
+      id : 3,
+      title : '成瀬瑛美',
+      subtitle : 'ハイテンションA-POPガール'
+    },
+    {
+      id : 4,
+      title : '最上もが',
+      subtitle : '金色の異端児'
+    },
+    {
+      id : 5,
+      title : '藤咲彩音',
+      subtitle : '踊ってみたら七変化！'
+    }
   ];
 })
